94f89bbee1f94f7ba823250b1d6e0a8fbf7029cd
ntaulya <netaaulya@gmail.com>
2022-06-14 06:17:57 +0700

    halaman blog dilengkapi

--- resources/views/pages/blog/blog1.blade.php
+++ resources/views/pages/blog/blog1.blade.php
@@ -1,18 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-
-<div class="container">
-    <div class="card" style="margin-top: 50px">
-        <img src="images/blog/blog.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-        <h5 class="card-title">The best service ever get</h5>
-        <p class="card-text">These men promptly escaped from maximum security stockade to the underground. The Brady Bunch the Brady Bunch.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In iaculis ante nisl, eu vestibulum nisl consequat sed. Integer nec magna vitae purus imperdiet rutrum. Proin ac turpis ac sem pharetra ullamcorper quis ac magna. Aliquam erat volutpat. Phasellus non erat ac nulla ornare placerat vitae et elit. In id commodo neque, malesuada porta mauris. Ut tristique ultrices ipsum, condimentum egestas nunc congue eget. Phasellus viverra ligula lacus, non vehicula diam rhoncus quis. Pellentesque cursus, felis ac porta commodo, est diam efficitur nunc, sed ultrices risus est tempor ligula. Curabitur erat augue, volutpat id auctor sed, bibendum eget metus. Suspendisse potenti. Nullam sodales a magna ullamcorper porttitor. Etiam eget ligula mi.
-            Donec quis turpis vel nisl hendrerit mattis. In eros nisl, venenatis non leo eleifend, dictum laoreet felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at nisl turpis. Aenean feugiat eleifend lorem, sed facilisis arcu rutrum eu. Nam sagittis semper mi ultrices dictum. Donec vitae nulla sem. Sed feugiat erat a elit bibendum sodales. Sed eu velit nisl. Sed convallis quam nec justo venenatis, nec placerat felis maximus. Duis auctor magna feugiat ligula faucibus vehicula. Nullam tincidunt efficitur velit a blandit. Proin tincidunt sit amet sapien eget aliquet. Donec non risus pharetra, fringilla diam non, lobortis odio. Proin bibendum lorem ullamcorper aliquam iaculis.
-        </p>
+    <div class="container">
+        <div class="card" style="margin-top: 50px">
+            <p style="text-align:justify;">
+                <img src="images/blog/blog.jpg" style="float:left; margin:0 9px 3px 0; width: 50%" />
+                <b style="font-size: 24px">The best service ever get</b>
+                <br>These men promptly escaped from maximum security stockade to the underground. The Brady Bunch the Brady
+                Bunch.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In iaculis ante nisl, eu vestibulum nisl consequat
+                sed. Integer nec magna vitae purus imperdiet rutrum. Proin ac turpis ac sem pharetra ullamcorper quis ac
+                magna. Aliquam erat volutpat. Phasellus non erat ac nulla ornare placerat vitae et elit. In id commodo
+                neque, malesuada porta mauris. Ut tristique ultrices ipsum, condimentum egestas nunc congue eget. Phasellus
+                viverra ligula lacus, non vehicula diam rhoncus quis. Pellentesque cursus, felis ac porta commodo, est diam
+                efficitur nunc, sed ultrices risus est tempor ligula. Curabitur erat augue, volutpat id auctor sed, bibendum
+                eget metus. Suspendisse potenti. Nullam sodales a magna ullamcorper porttitor. Etiam eget ligula mi.
+                Donec quis turpis vel nisl hendrerit mattis. In eros nisl, venenatis non leo eleifend, dictum laoreet felis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at nisl turpis. Aenean feugiat eleifend
+                lorem, sed facilisis arcu rutrum eu. Nam sagittis semper mi ultrices dictum. Donec vitae nulla sem. Sed
+                feugiat erat a elit bibendum sodales. Sed eu velit nisl. Sed convallis quam nec justo venenatis, nec
+                placerat felis maximus. Duis auctor magna feugiat ligula faucibus vehicula. Nullam tincidunt efficitur velit
+                a blandit. Proin tincidunt sit amet sapien eget aliquet. Donec non risus pharetra, fringilla diam non,
+                lobortis odio. Proin bibendum lorem ullamcorper aliquam iaculis.
+            </p>
         </div>
-    </div>
-</div>
-
-@endsection
+    @endsection
